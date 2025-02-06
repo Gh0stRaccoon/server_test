919a9f8be49879751f00ba0822bd17dd3ed93239
Joaquinossandon <joaquin.ossandon@inacapmail.cl>
2025-02-06 19:59:19 -0300

    add PostgreSQL connection and query endpoint to express application

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { default: pool } = require('./connection')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -16,6 +17,11 @@ app.get('/users', (req, res) => {
     { id: 1, name: 'Alice' },
     { id: 2, name: 'Bob' }
   ])
+})
+
+app.get('/hora', async (req, res) => {
+  const result = pool.query('SELECT NOW()')
+  res.json(result.rows)
 })
 
 app.listen(port, () => {
