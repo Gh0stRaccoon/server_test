dbe495aab5074640dbed7986964ae9b398518436
Joaquinossandon <joaquin.ossandon@inacapmail.cl>
2025-02-06 19:43:12 -0300

    add users route to express application

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ app.get('/about', (req, res) => {
   res.send('About Us')
 })
 
+app.get('/users', (req, res) => {
+  res.json([
+    { id: 1, name: 'Alice' },
+    { id: 2, name: 'Bob' }
+  ])
+})
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`)
 })
