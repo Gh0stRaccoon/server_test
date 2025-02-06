0d1544ad4c90c1fce19572c042516d74856d12af
Joaquinossandon <joaquin.ossandon@inacapmail.cl>
2025-02-06 20:18:15 -0300

    add CORS middleware to enable cross-origin requests

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const { default: pool } = require('./connection')
+const cors = require('cors')
 
 const app = express()
 const port = process.env.PORT || 3000
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
